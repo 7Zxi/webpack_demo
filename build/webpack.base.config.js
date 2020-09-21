ba506048feecc4038f0522ec00119656a3e14273
7Zxi <942749940@qq.com>
2020-09-21 14:22:36 +0800

    babel-loader的include属性url路径错误修改

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -115,7 +115,7 @@ module.exports = {
                         ]
                     }
                 },
-                include: path.resolve(__dirname, 'src'),// 只转化src目录下的js
+                include: path.resolve(__dirname, '../src'),// 只转化src目录下的js
                 exclude: /node_modules/  // 排除掉node_modules，优化打包速度
             },
             {
